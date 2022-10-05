37a485f13d7c32c1b22d3876e6915ba863833dcb
pythonubung <pythonubung@gmail.com>
2022-10-06 00:58:16 +0200

    Mostly done (controller as well)

--- src/main/java/meetingrooms/MeetingRoomController.java
+++ src/main/java/meetingrooms/MeetingRoomController.java
@@ -1,10 +1,17 @@
 package meetingrooms;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class MeetingRoomController {
 
-    private Office office;
+    // azért kell, hogy az Office attribútumait elérjük
+    private Office office = new Office();
+
+    // ?? IDE érdemes a Scanner-t példányosítani, az pufferelést megoldja?
+    // azért érdemes kiemelni, hogy ne kelljen többször példányosítani metódusokban
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -14,29 +21,17 @@ public class MeetingRoomController {
     }
         private void runMenu(){
             // Hány tárgyalót szeretne rögzíteni?
-            Scanner scanner = new Scanner(System.in);
+            //Scanner scanner = new Scanner(System.in);
             System.out.println("Hány darab tárgyaló adatait rögzítsük?");
                 int rooms = scanner.nextInt();
+                scanner.nextLine(); // azért, hogy utána az enter-t is bevegye
+            readMeetingRooms(rooms); // ezt jobb gomb-->Refactor-->Extract method -dal Idea kiszervezte, átneveztük
 
-            // Tárgyalók adatai: Név? Szélesség? Hosszúság (méter)?
-                //for (int parameters : rooms) {
-                for(int i=0; i<rooms; i++) {
-                    System.out.println("Tárgyaló neve?");
-                        String name = scanner.nextLine();
-                    System.out.println("Tárgyaló szélessége?");
-                        int width = scanner.nextInt();
-                    System.out.println("Tárgyaló hosszúsága?");
-                        int length = scanner.nextInt();
-
-                    // Az adatokat átadjuk Office addMeetingRoom-nak
-                    // Ő példányosítson új tárgyalót, és adja hozzá azt az osztály listájához
-                    Office office = new Office();
-                    office.addMeetingRoom(name, width, length);
-                }
-
+            // meghívja a printMenu()-t
+                printMenu();
                 // Test
 
-
+                    System.out.println(office.getMeetingRooms().size());
                 /*
                 for (int paramaters : rooms) {
                     System.out.println("Tárgyaló neve:");
@@ -51,9 +46,109 @@ public class MeetingRoomController {
 
         }
 
-        private void printMenu() {
+    private void readMeetingRooms(int rooms) {
+        // Tárgyalók adatai: Név? Szélesség? Hosszúság (méter)?
+        //for (int parameters : rooms) {
+        for(int i = 0; i< rooms; i++) {
+            System.out.println("Tárgyaló neve?");
+                String name = scanner.nextLine();
+                // just as "buffer" to not to skip to next nextInt right after
+                 scanner.nextLine();
+            System.out.println("Tárgyaló szélessége?");
+                int width = scanner.nextInt();
+                // just as "buffer" to not to skip to next nextInt right after
+                scanner.nextLine();
+            System.out.println("Tárgyaló hosszúsága?");
+                int length = scanner.nextInt();
+                // just as "buffer" to not to skip to next nextInt right after
+                scanner.nextLine();
+                // just as "buffer" to not to skip to next nextInt right after
+                // scanner.nextLine();
+
+            // Az adatokat átadjuk Office addMeetingRoom-nak
+            // Ő példányosítson új tárgyalót, és adja hozzá azt az osztály listájához
+            // EZ nem jó itt, mivel túl sokszor példányosítana... elejére osztálynak kiemeltük: Office office = new Office();
+            office.addMeetingRoom(name, width, length);
+        }
+    }
+
+    private void printMenu() {
+
+            System.out.println("1. Tárgyalók sorrendben");
+            System.out.println("2. Tárgyalók visszafele sorrendben");
+            System.out.println("3. Minden második tárgyaló");
+            System.out.println("4. Területek");
+            System.out.println("5. Keresés pontos név alapján");
+            System.out.println("6. Keresés névtöredék alapján");
+            System.out.println("7. Keresés terület alapján");
+            System.out.println("8. Kilépés");
+
+            System.out.println("Kérem a menüpont számát!");
+            int whichMethod = scanner.nextInt();
+            scanner.nextLine();
+
+        chooseMenuItem(whichMethod); // ezt is kiszervezte Refactorral Idea
+
+    }
+
+    private void chooseMenuItem(int whichMethod) {
+        if (whichMethod ==1) {
+            printNames(                 // én írom meg, ciklussal végigmegy a lista elemein és kiírja őket szépen
+                        office.getMeetingRooms()
+            );
+        }
+        if (whichMethod ==2) {
+            printNames( office.getMeetingRoomsInReverseOrder() );
+        }
+        if (whichMethod ==3) {
+            System.out.println("Ha 1-est ad be a páratlanok jönnek, ha 2-est a párosak. Melyik legyen?");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+
+            printNames(office.getEverySecondMeetingRoom(number));
+        }
+        if (whichMethod ==4) {
+            printMeetingRooms(office.getMeetingRooms());
+        }
+        if (whichMethod ==5) {
 
         }
+        if (whichMethod ==6) {
+
+        }
+        if (whichMethod ==7) {
+
+        }
+        if (whichMethod ==8) {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    // kiiratásra mi írjuk meg
+    private void printNames(List<MeetingRoom> listOfRooms) {
+        for (MeetingRoom items : listOfRooms) {
+            System.out.println(items.getName());
+        }
+    }
+
+    // kiiratásra mi írjuk meg, a tárgyalók minden adatát szépen kiírja
+    private void printMeetingRooms(List<MeetingRoom> meetingRooms){
+        for (MeetingRoom item : meetingRooms) {
+            System.out.println(
+                                "név: " + item.getName() + " , " +
+                                "szélessége: " + item.getWidth() + " méter, " +
+                                "hossza: " + item.getLength() + " méter,  " +
+                                "területe: " + item.getArea() + " m2."
+            );
+        }
+
+    }
+
+
 }
 
 
--- src/main/java/meetingrooms/Office.java
+++ src/main/java/meetingrooms/Office.java
@@ -20,27 +20,89 @@ public class Office {
     }
 
     public List<MeetingRoom> getMeetingRoomsInReverseOrder() {
-        return meetingRooms;
+        List<MeetingRoom> reverseRooms = new ArrayList<>();
+        for(int i=(meetingRooms.size()-1); i>=0; i--) {
+                reverseRooms.add(meetingRooms.get(i)); //.getName());
+            }
+
+
+        return reverseRooms;
     }
 
     public List<MeetingRoom> getEverySecondMeetingRoom(int number) {
-        return meetingRooms;
+        // egy új listát deklarálunk, ide fogjuk pakolni a találatokat és ez lesz return-ben is
+        List<MeetingRoom> secondRooms = new ArrayList<>();
+
+        // végigmegyünk a meetinRooms listán
+        for(int i=0; i< meetingRooms.size(); i++) {
+            // páros = ha felhasználó 2-t adott meg páros
+            if(i%2==0 && number==2) {
+                secondRooms.add(meetingRooms.get(i));
+            }
+            else if(i%2==1 && number==1) { // ha a felhasználó 1-et adott meg páratlan
+                secondRooms.add(meetingRooms.get(i));
+            }
+            else {} // hibakezelés helyett...
+        }
+        return secondRooms;
     }
 
-    /*
+
     public MeetingRoom getMeetingRoomWithGivenName(String name) {
-        Office office = new Office();
-        office.getMeetingRooms(name);
-        MeetingRoom meetingroom = new MeetingRoom();
-        return meetingroom;
+
+    /* EZ ÉRDEKES, foreach jó lenne, sima for nem??
+        // előbb a List tárgyalókon végigmegyünk keresni
+        for (int i=0; i<meetingRooms.size(); i++) {
+            if(meetingRooms.get(i).getName().equals(name)) {
+                // megtaláltuk, adja vissza az objektumot
+                return meetingRooms;
+
+            }
+        }
+        // ha nem találta meg, akkor egy null referenciát adunk vissza
+        return null;
+     */
+        // foreach: objektum típusú item végigmegy a LIST -en!
+        for(MeetingRoom item : meetingRooms) {
+            // ha az aktuálisan vizsgált eleme neve egyezik a kapott metódusparaméter name-el, akkor return
+            if(item.getName().equals(name)) {
+                return item; // egy objektumot ad így vissza! tehát jó
+            }
+        }
+        return null; // ha nem találta meg, akkor null-t ad vissza
     }
-    */
+
+
     public List<MeetingRoom> getMeetingRoomsWithGivenNamePart(String namePart) {
-        return meetingRooms;
+        //egy új lista, ebbbe gyűjtjük a találatokat és return is ezt adja vissza majd
+        List<MeetingRoom> givenNamePartRooms = new ArrayList<>();
+
+        // itt most a listában megyünk végig
+        for(int i=0; i<meetingRooms.size(); i++) {
+            // ha az aktuális sorszámú elem name attribútuma részben egyezik a kapott metódusparaméter namePart-el, akkor return
+            if(meetingRooms.get(i).getName().contains(namePart)) {
+                givenNamePartRooms.add(meetingRooms.get(i)); // az aktuális elemét a listának bedobjuk, mivel azt találta meg
+            }
+        }
+        //return null; // ha nem találta meg, akkor null-t ad vissza
+        return givenNamePartRooms; // ha nem volt találat akkor az üres lista megy vissza
+
     }
+
+
 
     public List<MeetingRoom> getMeetingRoomsWithAreaLargerThan(int area) {
-        return meetingRooms;
+        // találati lista
+        List<MeetingRoom> largeRoom = new ArrayList<>();
+
+        for (MeetingRoom items : meetingRooms) {
+            if(items.getArea() > area) {
+                largeRoom.add(items);
+            }
+        }
+
+        //return meetingRooms;
+        return largeRoom;
     }
 
 }
